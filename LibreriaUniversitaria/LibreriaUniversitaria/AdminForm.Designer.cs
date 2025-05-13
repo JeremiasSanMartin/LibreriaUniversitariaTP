fc85e149e77db80670e3ea9edb3d118b59d1f74f
Moya328 <tomasmoyano05@hotmail.com>
2025-05-13 19:33:21 -0300

    IMPLEMENTACION LOGIN
    Se realizo un login basico con la utilizacion de dos capas (Logica y Presentacion) con datos hardcodeados y una aproximacion para una futura base de datos
    
    Faltaria: Pruebas unitarias, excepciones y mejorar la interfaz de los formularios con nuevos metodos para los roles correspondientes.

--- LibreriaUniversitaria/LibreriaUniversitaria/AdminForm.Designer.cs
+++ LibreriaUniversitaria/LibreriaUniversitaria/AdminForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Presentacion
 {
-    partial class LoginForm
+    partial class AdminForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(662, 180);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_salir.TabIndex = 0;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(749, 215);
+            this.Controls.Add(this.btn_salir);
+            this.Name = "AdminForm";
+            this.Text = "AdminForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_salir;
     }
 }
 
---
+++ LibreriaUniversitaria/Persistencia/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Persistencia;
+using DAL;
+
+namespace Persistencia
+{
+    public class Usuario
+    {
+        public List<Entidades.Usuario> Listar()
+        {
+            Conexion conexion = new Conexion();
+
+            List<Entidades.Usuario> usuarios = new List<Entidades.Usuario>();
+
+            DataTable datatable = conexion.LeerPorComando("COLOCAR PROCEDIMIENTO ALMACENADO");
+
+            foreach (DataRow fila in datatable.Rows) 
+            {
+                Entidades.Usuario unUsuario = new Entidades.Usuario();
+
+                unUsuario.Clave = fila["clave"].ToString();
+                unUsuario.UserName = fila["usuario"].ToString();
+
+                usuarios.Add(unUsuario);
+            }
+
+            return usuarios;
+        }
+    }
+
+    
+}
