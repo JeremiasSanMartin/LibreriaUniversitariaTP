4be272f9ec05bd882c0994fd196949f49005d49a
jereswaxd <jeresanmartin820@gmail.com>
2025-06-12 20:35:17 -0300

    Update LoginTest.cs

--- LibreriaUniversitaria/LibreriaTest/LoginTest.cs
+++ LibreriaUniversitaria/LibreriaTest/LoginTest.cs
@@ -12,17 +12,11 @@ namespace UnitTestProject1
         public void TestAdmin()
         {
             var usuario = new UsuarioLogica();
-<<<<<<< Updated upstream
             string rol;
             bool resultado = usuario.loguearse("miardg", "1234", out rol);
 
             Debug.WriteLine(rol);
             Assert.IsTrue(resultado);
-=======
-            bool loginExitoso = usuario.loguearse("miardg", "1234", out string rol);
-
-            Assert.IsTrue(loginExitoso);
->>>>>>> Stashed changes
             Assert.AreEqual("Administrador", rol);
         }
 
@@ -30,17 +24,11 @@ namespace UnitTestProject1
         public void TestBiblotecario()
         {
             var usuario = new UsuarioLogica();
-<<<<<<< Updated upstream
             string rol;
             bool resultado = usuario.loguearse("Bibliotecario", "1234", out rol);
 
             Debug.WriteLine(rol);
             Assert.IsTrue(resultado);
-=======
-            bool loginExitoso = usuario.loguearse("Bibliotecario", "1234", out string rol);
-
-            Assert.IsTrue(loginExitoso);
->>>>>>> Stashed changes
             Assert.AreEqual("Bibliotecario", rol);
         }
 
@@ -48,36 +36,23 @@ namespace UnitTestProject1
         public void TestInvalido()
         {
             var usuario = new UsuarioLogica();
-<<<<<<< Updated upstream
             string rol;
             bool resultado = usuario.loguearse("fakeuser", "wrong", out rol);
 
             Debug.WriteLine(rol);
             Assert.IsFalse(resultado);
             Assert.IsNull(rol); // Si no existe, debe ser null  
-=======
-            bool loginExitoso = usuario.loguearse("fakeuser", "wrong", out string rol);
-
-            Assert.IsFalse(loginExitoso);
-            Assert.IsNull(rol); // Si no existe, debe ser null
->>>>>>> Stashed changes
         }
 
         [TestMethod]
         public void TestRolErroneo()
         {
             var usuario = new UsuarioLogica();
-<<<<<<< Updated upstream
             string rol;
             bool resultado = usuario.loguearse("admin", "1234", out rol);
 
             Debug.WriteLine(rol);
             Assert.IsFalse(resultado);
-=======
-            bool loginExitoso = usuario.loguearse("admin", "1234", out string rol);
-
-            Assert.IsFalse(loginExitoso);
->>>>>>> Stashed changes
             Assert.IsNull(rol);
         }
     }
