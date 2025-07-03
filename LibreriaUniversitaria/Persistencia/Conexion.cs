cafde28d3c8d74b6a248901f478a4c7d1bb9c3bc
jereswaxd <jeresanmartin820@gmail.com>
2025-07-03 20:06:22 -0300

    quitado 01 de conexion
    se quito el 01 de la conexion, dejado por error

--- LibreriaUniversitaria/Persistencia/Conexion.cs
+++ LibreriaUniversitaria/Persistencia/Conexion.cs
@@ -24,7 +24,7 @@ namespace DAL
          */
         private void conectar()
         {   // HACK: Cadena de conexión hardcodeada. Luego ponerla como parametro de configuración del proyecto u otra alternativa.
-            strCadenaDeConexion = @"Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = BibliotecaDB; Data Source = . \SQLEXPRESS01";
+            strCadenaDeConexion = @"Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = BibliotecaDB; Data Source = . \SQLEXPRESS";
 
             //Instanció un objeto del tipo SqlConnection
             objConexion = new SqlConnection();
